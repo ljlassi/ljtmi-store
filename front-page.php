1bdf0577a6babdebff33ddd7b07aeffb74d3b8e8
Lassi Jääskeläinen <lassijaa@gmail.com>
2020-03-02 16:16:35 +0200

    More work on frontpage

--- front-page.php
+++ front-page.php
@@ -91,6 +91,47 @@
     </div>
 </div>
 
+<div class="row">
+
+    <div class="col-md-1 col-sm-0">
+    </div>
+
+    <div class="col-md-10 col-sm-0">
+
+
+        <div class="row">
+            <div class="col-md-6 col-sm-12">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="<?php echo get_template_directory_uri();?>/img/motherboard.jpg" class="img-main-pages" alt="cpu">
+                    </div>
+                    <div class="col-md-6">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales neque sodales ut etiam. Molestie nunc non blandit massa enim. Tellus molestie nunc non blandit massa enim nec. Sollicitudin ac orci phasellus egestas. Iaculis urna id volutpat lacus laoreet non</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="<?php echo get_template_directory_uri();?>/img/case.jpg" class="img-main-pages" alt="gpu">
+                    </div>
+                    <div class="col-md-6">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales neque sodales ut etiam. Molestie nunc non blandit massa enim. Tellus molestie nunc non blandit massa enim nec. Sollicitudin ac orci phasellus egestas. Iaculis urna id volutpat lacus laoreet non</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-1 col-sm-0">
+        </div>
+    </div>
+</div>
+
+
+<div class="row">
+    <div class="col-12 spacing-frontpage">
+    </div>
+</div>
+
 
 
 <?php get_footer(); ?>
