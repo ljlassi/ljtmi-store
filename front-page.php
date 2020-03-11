23c25f3f08c393e8107faab009f72b098d6bea34
Lassi Jääskeläinen <lassijaa@gmail.com>
2020-03-11 15:58:41 +0200

    Worked on frontpage

--- front-page.php
+++ front-page.php
@@ -15,7 +15,7 @@
             <div class="row">
                 <div class="col-md-2 col-sm-0 "></div>
                 <div class="col-md-8 col-sm-12">
-                    <div class="main p-3 d-flex justify-content-center">
+                    <div class="main p-3 d-flex justify-content-center content-main">
                         <?php
 
                         /**
@@ -30,10 +30,15 @@
                         }
                         ?>
                     </div>
-                    <div class="main p-3 d-flex justify-content-center">
+                    <div class="main p-3 d-flex justify-content-center content-main">
                         <?php
                         echo do_shortcode('[featured_products per_page=\"3\" columns=\"3\"]')
                         ?>
+
+                    </div>
+
+                    <div class="main p-1 d-flex justify-content-center content-main">
+                        <h3><a href="<?php echo get_site_url(); ?>/kauppa/">Täysi tuotelista</a></h3>
 
                     </div>
                 </div>
@@ -56,27 +61,30 @@
             </div>
 
             <div class="col-md-10 col-sm-0">
+                <div class="main p-3 pb-5 d-flex justify-content-center content-main">
+                    <h3>Päätuotekategoriat</h3>
 
+                </div>
 
         <div class="row">
-            <div class="col-md-6 col-sm-12 tofadein-first">
+            <div class="col-md-6 col-sm-12 tofadein-first content-main">
                 <div class="row">
                 <div class="col-md-6">
                     <img src="<?php echo get_template_directory_uri();?>/img/cpu.jpg" class="img-main-pages" alt="cpu">
                 </div>
                     <div class="col-md-6">
-                        <h3>Prosessorit</h3>
+                        <a href="<?php echo get_site_url(); ?>/tuote-osasto/prosessorit/"><h4>Prosessorit</h4></a>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales neque sodales ut etiam. Molestie nunc non blandit massa enim. Tellus molestie nunc non blandit massa enim nec. Sollicitudin ac orci phasellus egestas. Iaculis urna id volutpat lacus laoreet non</p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-sm-12 tofadein-second">
+            <div class="col-md-6 col-sm-12 tofadein-second content-main">
                 <div class="row">
                 <div class="col-md-6">
                     <img src="<?php echo get_template_directory_uri();?>/img/gpu.png" class="img-main-pages" alt="gpu">
                 </div>
                 <div class="col-md-6">
-                    <h3>Grafiikkakortit</h3>
+                    <a href="<?php echo get_site_url(); ?>/tuote-osasto/grafiikkakortit/"><h4>Grafiikkakortit</h4></a>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales neque sodales ut etiam. Molestie nunc non blandit massa enim. Tellus molestie nunc non blandit massa enim nec. Sollicitudin ac orci phasellus egestas. Iaculis urna id volutpat lacus laoreet non</p>
                 </div>
                 </div>
@@ -102,24 +110,24 @@
 
 
         <div class="row">
-            <div class="col-md-6 col-sm-12 tofadein-first">
+            <div class="col-md-6 col-sm-12 tofadein-first content-main">
                 <div class="row">
                     <div class="col-md-6">
                         <img src="<?php echo get_template_directory_uri();?>/img/motherboard.jpg" class="img-main-pages" alt="cpu">
                     </div>
                     <div class="col-md-6">
-                        <h3>Emolevyt</h3>
+                        <a href="<?php echo get_site_url(); ?>/tuote-osasto/emolevyt/"><h4>Emolevyt</h4></a>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales neque sodales ut etiam. Molestie nunc non blandit massa enim. Tellus molestie nunc non blandit massa enim nec. Sollicitudin ac orci phasellus egestas. Iaculis urna id volutpat lacus laoreet non</p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-sm-12 tofadein-second">
+            <div class="col-md-6 col-sm-12 tofadein-second content-main">
                 <div class="row">
                     <div class="col-md-6">
                         <img src="<?php echo get_template_directory_uri();?>/img/case.jpg" class="img-main-pages" alt="gpu">
                     </div>
                     <div class="col-md-6">
-                        <h3>Kotelot</h3>
+                        <a href="<?php echo get_site_url(); ?>/tuote-osasto/kotelot/"><h4>Kotelot</h4></a>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales neque sodales ut etiam. Molestie nunc non blandit massa enim. Tellus molestie nunc non blandit massa enim nec. Sollicitudin ac orci phasellus egestas. Iaculis urna id volutpat lacus laoreet non</p>
                     </div>
                 </div>
